7edca37917e57d3218db48872fc4c2921e26a18f
Marcus <marcus.shimotsu@gmail.com>
2021-10-19 10:08:04 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(nameArray) {
+    let arr = [];
+    for(let i = 0; i <nameArray.length; i++) {
+        arr.push(`Thank you, ${nameArray[i]}, for the wonderful surprise gift!`);
+    }
+    return arr;
+}
+function countDown(n) {
+    while(n>=0) {
+        console.log(n);
+        n--;
+    }
+}
